3d667b9a810209474a05e1878510b2f7339d994d
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-12-21 23:40:49 -0300

    Config NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,16 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2634393095f9b3',
+    pass: '83c1967ca6014e',
+  },
+  default: {
+    from: 'Gustavo Constantini <noreply@constantinibarber.com>',
+  },
+};
+
+/*
+  * Amazon SES, Mailgun, Aparkpost, Madril(MailChimp), Mailtrap para desenvolvimento
+*/
